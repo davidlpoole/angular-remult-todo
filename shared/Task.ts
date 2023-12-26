ff20b8ff894f02006c84aeb30dbf4f3852252db0
David Poole <14233215+davidlpoole@users.noreply.github.com>
2023-12-26 16:39:04 +1300

    Add form validation

--- shared/Task.ts
+++ shared/Task.ts
@@ -1,4 +1,4 @@
-import { Entity, Fields } from 'remult';
+import { Entity, Fields, Validators } from 'remult';
 
 @Entity('tasks', {
   allowApiCrud: true,
@@ -7,7 +7,16 @@ export class Task {
   @Fields.cuid()
   id = '';
 
-  @Fields.string()
+  // @Fields.string({
+  //   validate: Validators.required,
+  // })
+  // title = '';
+  @Fields.string<Task>({
+    validate: (task) => {
+      if (!task.title) throw 'Should not be empty';
+      if (task.title.length < 3) throw 'Too Short';
+    },
+  })
   title = '';
 
   @Fields.boolean()
